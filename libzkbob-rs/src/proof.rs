45de7692373f3902830aa52e45c2c84feeb5ef74
Alexander Filippov <aleksander.fill@gmail.com>
2023-04-11 10:58:32 +0300

    Refactor code

--- libzkbob-rs/src/proof.rs
+++ libzkbob-rs/src/proof.rs
@@ -21,6 +21,7 @@ pub fn prove_tx<P, E>(
     pool_params: &P,
     transfer_pub: TransferPub<E::Fr>,
     transfer_sec: TransferSec<E::Fr>,
+    precomputed: &Option<PrecomputedData<E::Fr>>,
 ) -> (Vec<Num<E::Fr>>, Proof<E>)
 where
     P: PoolParams<Fr = E::Fr>,
@@ -30,25 +31,12 @@ where
         c_transfer(&public, &secret, pool_params);
     };
 
-    prove(params, &transfer_pub, &transfer_sec, circuit)
-}
-
-pub fn prove_tx_precomputed<P, E>(
-    params: &Parameters<E>,
-    pool_params: &P,
-    transfer_pub: TransferPub<E::Fr>,
-    transfer_sec: TransferSec<E::Fr>,
-    precomputed: &PrecomputedData<E::Fr>,
-) -> (Vec<Num<E::Fr>>, Proof<E>)
-where
-    P: PoolParams<Fr = E::Fr>,
-    E: Engine,
-{
-    let circuit = |public, secret| {
-        c_transfer(&public, &secret, pool_params);
-    };
-
-    prove_precomputed(params, &transfer_pub, &transfer_sec, circuit, precomputed)
+    match precomputed {
+        Some(precomputed) => {
+            prove_precomputed(params, &transfer_pub, &transfer_sec, circuit, precomputed)
+        },
+        None => prove(params, &transfer_pub, &transfer_sec, circuit)
+    }
 }
 
 pub fn prove_tree<P, E>(
@@ -56,6 +44,7 @@ pub fn prove_tree<P, E>(
     pool_params: &P,
     tree_pub: TreePub<E::Fr>,
     tree_sec: TreeSec<E::Fr>,
+    precomputed: &Option<PrecomputedData<E::Fr>>,
 ) -> (Vec<Num<E::Fr>>, Proof<E>)
 where
     P: PoolParams<Fr = E::Fr>,
@@ -65,25 +54,12 @@ where
         tree_update(&public, &secret, pool_params);
     };
 
-    prove(params, &tree_pub, &tree_sec, circuit)
-}
-
-pub fn prove_tree_precomputed<P, E>(
-    params: &Parameters<E>,
-    pool_params: &P,
-    tree_pub: TreePub<E::Fr>,
-    tree_sec: TreeSec<E::Fr>,
-    precomputed: &PrecomputedData<E::Fr>,
-) -> (Vec<Num<E::Fr>>, Proof<E>)
-where
-    P: PoolParams<Fr = E::Fr>,
-    E: Engine,
-{
-    let circuit = |public, secret| {
-        tree_update(&public, &secret, pool_params);
-    };
-
-    prove_precomputed(params, &tree_pub, &tree_sec, circuit, precomputed)
+    match precomputed {
+        Some(precomputed) => {
+            prove_precomputed(params, &tree_pub, &tree_sec, circuit, precomputed)
+        },
+        None => prove(params, &tree_pub, &tree_sec, circuit)
+    }
 }
 
 pub fn prove_delegated_deposit<P, E>(
@@ -91,6 +67,7 @@ pub fn prove_delegated_deposit<P, E>(
     pool_params: &P,
     d_pub: DelegatedDepositBatchPub<E::Fr>,
     d_sec: DelegatedDepositBatchSec<E::Fr>,
+    precomputed: &Option<PrecomputedData<E::Fr>>,
 ) -> (Vec<Num<E::Fr>>, Proof<E>)
 where
     P: PoolParams<Fr = E::Fr>,
@@ -100,24 +77,10 @@ where
         check_delegated_deposit_batch(&public, &secret, pool_params);
     };
 
-    prove(params, &d_pub, &d_sec, circuit)
+    match precomputed {
+        Some(precomputed) => {
+            prove_precomputed(params, &d_pub, &d_sec, circuit, precomputed)
+        },
+        None => prove(params, &d_pub, &d_sec, circuit)
+    }
 }
-
-pub fn prove_delegated_deposit_precomputed<P, E>(
-    params: &Parameters<E>,
-    pool_params: &P,
-    d_pub: DelegatedDepositBatchPub<E::Fr>,
-    d_sec: DelegatedDepositBatchSec<E::Fr>,
-    precomputed: &PrecomputedData<E::Fr>,
-) -> (Vec<Num<E::Fr>>, Proof<E>)
-where
-    P: PoolParams<Fr = E::Fr>,
-    E: Engine,
-{
-    let circuit = |public, secret| {
-        check_delegated_deposit_batch(&public, &secret, pool_params);
-    };
-
-    prove_precomputed(params, &d_pub, &d_sec, circuit, precomputed)
-}
-
